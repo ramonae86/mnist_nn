c60786683cc9720c3a4db6e0a3744727f8827948
ramonae86 <ramonzhi@gmail.com>
2019-08-26 16:49:30 -0400

    git credential test

--- test.py
+++ test.py
@@ -67,7 +67,6 @@ a = np.array([1, 2555, 99142])
 print(biases_to_binary(a, scale = 1, num_bytes=2))
 print(biases_to_binary(a, scale = 1, num_bytes=4))
 
-
 b = 234
 print(format(b, '04x').upper())
 
